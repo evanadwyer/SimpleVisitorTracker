3ad3f4b5eb10243206fa2be1d84e2f91179bc845
evanadwyer <evanadwyer@gmail.com>
2023-02-17 20:32:28 -0800

    add hangout option

--- app/src/main/java/com/evanadwyer/simplevisitortracker/VisitType.kt
+++ app/src/main/java/com/evanadwyer/simplevisitortracker/VisitType.kt
@@ -3,5 +3,6 @@ package com.evanadwyer.simplevisitortracker
 enum class VisitType(val value: String) {
     EVENT("Event!"),
     GEAR_PICKUP("Gear Pickup!"),
-    GEAR_DROP_OFF("Gear Drop-off!");
+    GEAR_DROP_OFF("Gear Drop-off!"),
+    HANGOUT("Hangout!");
 }
