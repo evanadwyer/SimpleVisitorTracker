e38eb9ff735ff9a6b9e92cc958834f5ca4f073e7
evanadwyer <evanadwyer@gmail.com>
2023-02-17 20:40:17 -0800

    create one service to use across operations

--- app/src/main/java/com/evanadwyer/simplevisitortracker/sheets/SheetsService.kt
+++ app/src/main/java/com/evanadwyer/simplevisitortracker/sheets/SheetsService.kt
@@ -14,23 +14,24 @@ import com.google.api.services.sheets.v4.model.ValueRange
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-suspend fun appendValues(
-    context: Context,
-    spreadsheetId: String,
-    range: String,
-    valueInputOption: String,
-    values: List<List<Any>>
-): AppendValuesResponse? {
-    val scopes = listOf(SheetsScopes.SPREADSHEETS)
-    val credential = GoogleAccountCredential.usingOAuth2(context, scopes)
-    credential.selectedAccount = GoogleSignIn.getLastSignedInAccount(context)?.account
-    val service = Sheets.Builder(
-        NetHttpTransport(),
-        GsonFactory.getDefaultInstance(),
-        credential
-    )
-        .setApplicationName("SimpleVisitorTracker")
-        .build()
+class SheetsService(
+    context: Context
+) {
+
+    private val service: Sheets
+
+    init {
+        val scopes = listOf(SheetsScopes.SPREADSHEETS)
+        val credential = GoogleAccountCredential.usingOAuth2(context, scopes)
+        credential.selectedAccount = GoogleSignIn.getLastSignedInAccount(context)?.account
+        service = Sheets.Builder(
+            NetHttpTransport(),
+            GsonFactory.getDefaultInstance(),
+            credential
+        )
+            .setApplicationName("SimpleVisitorTracker")
+            .build()
+    }
 
     var response: AppendValuesResponse? = null
     try {
